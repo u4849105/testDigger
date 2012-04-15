862d9f035ca476943f584413f9a112954a712ba1
yunlong liu <u4849105@anu.edu.au>
2012-04-15 11:35:28 +1000

    test git

---
+++ src/main/java/com/agical/golddigger/gui/testGit.java
@@ -0,0 +1,13 @@
+package com.agical.golddigger.gui;
+
+public class testGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ ///asdgasdf
+	}
+
+}
